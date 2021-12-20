bb2fd6c50537eb6f536b18792f4901e2ef6d3763
Diogo Cardoso <diogocardoso92@outlook.com>
2021-12-20 00:39:58 +0000

    fix global styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -42,8 +42,8 @@ export default function App({ Component, pageProps }: AppProps) {
           `,
         }}
       />
-      <RootStyles />
       <ThemeProvider theme={theme}>
+        <RootStyles />
         <Component {...pageProps} />
       </ThemeProvider>
     </>
